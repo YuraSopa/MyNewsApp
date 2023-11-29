7b848b0f878cea1967bd172a09a7e848d9f1d17e
YuraSopa <aspiresopa@gmail.com>
2023-11-29 21:56:04 +0200

    added retrofit added NewsApi and Dto

---
+++ app/src/main/java/com/example/mnewsapp/Source.kt
@@ -0,0 +1,6 @@
+package com.example.mnewsapp
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/example/mnewsapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.mnewsapp.util
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "e64787ae0a1f47a7bec5e4b35bbcd7fe"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
