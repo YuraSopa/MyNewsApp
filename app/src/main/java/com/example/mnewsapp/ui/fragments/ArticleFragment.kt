a4d915bc8f914ebd77a625b56a487efee49b72f2
YuraSopa <aspiresopa@gmail.com>
2023-11-29 20:43:00 +0200

    create bottom navigation menu

---
+++ app/src/main/java/com/example/mnewsapp/ui/fragments/ArticleFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+}
---
+++ app/src/main/java/com/example/mnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,8 @@
+package com.example.mnewsapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.mnewsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+    
+}
